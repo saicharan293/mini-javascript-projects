af434718bd2f5b5b16bbd4b098c365d331b986be
sai <2493saicharan@gmail.com>
2024-01-08 23:50:12 +0530

    4project

---
+++ 4SoundBoard/script.js
@@ -0,0 +1,19 @@
+const sounds=['applause','boo','gasp','tada','victory','wrong']
+
+sounds.forEach((sound)=>{
+    const btn=document.createElement('button')
+    btn.classList.add('btn');
+    btn.innerText=sound;
+    btn.addEventListener('click',()=>{
+        stopMusic()
+        document.getElementById(sound).play();
+    })
+    document.getElementById('buttons').appendChild(btn);
+})
+function stopMusic(){
+    sounds.forEach(sound=>{
+        const song=document.getElementById(sound);
+        song.pause();
+        song.currentTime=0;
+    })
+}
